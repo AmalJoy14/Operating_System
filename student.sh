7ee6f4385b5ebdfd3aebb627211e085e5e10548a
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-02-05 11:11:08 +0530

    Add files via upload

---
+++ student.sh
@@ -0,0 +1,11 @@
+echo "Enter your name: "
+read name
+echo "Enter your batch:"
+read batch
+echo "Enter your semester:"
+read sem
+echo "Enter your RollNo:"
+read rollno
+echo "Enter your register no.:"
+read rno
+echo -e "Name : $name \nBatch :$batch \nSem : $sem \nRollno: $rollno \nRegister No.: $rno"
