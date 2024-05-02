3f307d4cf5d68b78f75ca711c89023f53ef00d63
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-05-02 06:34:55 +0530

    Update NewSharedMemory.c

--- NewSharedMemory.c
+++ NewSharedMemory.c
@@ -57,3 +57,42 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+
+#define SEGSIZE 100
+
+int main(int argc, char *argv[]) {
+    int shmid, key = ftok(".", 's');
+    char *segptr, buff[] = "Hello";
+
+    shmid = shmget(key, SEGSIZE, IPC_CREAT | IPC_EXCL | 0666);
+    if (shmid == -1 && (shmid = shmget(key, SEGSIZE, 0)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    segptr = (char*)shmat(shmid, 0, 0);
+    if (segptr == (char*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    strcpy(segptr, buff);
+    printf("Data in shared memory: %s\n", segptr);
+
+    shmdt(segptr);
+    shmctl(shmid, IPC_RMID, 0);
+
+    return 0;
+}
+
+*/
